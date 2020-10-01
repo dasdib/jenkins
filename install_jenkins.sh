68547b3585dce898ae1147a7b017552150225c5e
Dibyendu Das <ddas446@its.jnj.com>
2020-10-02 01:13:59 +0530

    Update jenkins installation

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -3,10 +3,12 @@
 # this script is only tested on ubuntu xenial
 
 # Install git
+echo 'Git installation start'
 apt-get update -y
 apt-get install git-core
 git config --global user.name "Dibyendu" 
 git config --global user.email "dasdib@gmail.com"
+echo 'Git installation completed'
 
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
@@ -14,20 +16,27 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-apt-get update
+
 apt-get install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
 systemctl start docker
 usermod -aG docker ubuntu
+
+echo 'Docker installation Completed'
+
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
+echo 'Docker installation Completed'
+
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
 
+
 # Git clone 
 git clone https://github.com/dasdib/jenkins.git
+echo 'Git cloned'
 
 cd jenkins
 docker-compose up -d
